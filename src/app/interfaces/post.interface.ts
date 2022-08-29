89e85aec3c7a171770a5d782d18ec6e540b2c388
domdutton <ddd94608@gmail.com>
2022-08-29 09:24:26 -0700

    Update post.interface.ts

--- src/app/interfaces/post.interface.ts
+++ src/app/interfaces/post.interface.ts
@@ -1,8 +1,10 @@
-export interface Post { 
-    $key: string;
-    author: string;
-    title: string;
-    content: string;
-    posted: Date;
-    edited: Date;
-  }
+export interface Post {
+  id: string;
+  author: string;
+  title: string;
+  content: string;
+  likes: Array<string>;
+  dislikes: Array<string>;
+  posted: Date;
+  edited: Date;
+}
