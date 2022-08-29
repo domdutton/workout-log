6b1405301a45690a91168553f1edccf802460b51
domdutton <ddd94608@gmail.com>
2022-08-29 06:28:47 -0700

    Update plan.interface.ts

--- src/app/interfaces/plan.interface.ts
+++ src/app/interfaces/plan.interface.ts
@@ -1,5 +1,8 @@
-export interface Plan { 
-    $key: string;
-    author: string;
-    title: string;
-  }
+export interface Plan {
+  id: string;
+  author: string;
+  title: string;
+  description: string;
+  exercises: Array<string>;
+  logs: Array<{ eid: string; date: Date; value: number }>;
+}
